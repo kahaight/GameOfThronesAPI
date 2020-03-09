186d7bb8af2c598c38c98dce4e4da09e83a02e8f
Konrad Haight <konradhaight@gmail.com>
2020-03-09 10:55:30 -0400

    same file as Marshall

--- GoTAPI.Services/CharacterService.cs
+++ GoTAPI.Services/CharacterService.cs
@@ -1,8 +1,5 @@
 ﻿using GoTAPI.Data.DataClasses;
-<<<<<<< HEAD
 using GoTAPI.Models;
-=======
->>>>>>> 29ec58678564f71f831f279818003d419b557dcf
 using GoTAPI.Models.CharacterModels;
 using System;
 using System.Collections.Generic;
@@ -29,10 +26,10 @@ namespace GoTAPI.Services
                     Name = model.Name,
                     Alive = model.Alive,
                     EpisodeOfDeath = model.EpisodeOfDeath,
-                    House = model.House,
                     Gender = model.Gender,
                     Actor = model.Actor,
-                    CauseOfDeath = model.CauseofDeath
+                    CauseOfDeath = model.CauseOfDeath,
+                    HouseId = model.HouseId
                 };
             using (var ctx=new ApplicationDbContext())
             {
@@ -42,27 +39,19 @@ namespace GoTAPI.Services
 
         }
 
-<<<<<<< HEAD
         public IEnumerable<CharacterListItem> ReadCharacters()
         {
             using (var ctx = new ApplicationDbContext())
             {
-                var query = ctx.Characters.Where(e=>e.OwnerId==_userId).Select(
-                    e=>
+                var query = ctx.Characters.Select(
+                    e =>
                         new CharacterListItem
                         {
-                            CharacterId=e.CharacterId,
-
-                        })
+                            Name = e.Name,
+                        });
+                return query.ToArray();
             }
         }
-=======
-        //public IEnumberable<CharacterListItem> ReadCharacters()
-        //{
-
-        //}
-
->>>>>>> 29ec58678564f71f831f279818003d419b557dcf
         //public CharacterDetail ReadCharacterById()
         //{
 
